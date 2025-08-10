67db291cf3d524d6c7849878a5001a137868bb24
a a <linksec013@gmail.com>
2025-08-10 09:49:43 +0100

    C: Finished CH3 L1!

--- main.py
+++ main.py
@@ -3,13 +3,28 @@ def get_book_text(filepath):
         file_contents = f.read()
         return file_contents
 
-def main():
-    from stats import words_in_book
-    words_in_book(get_book_text("books/frankenstein.txt"))
-    from stats import character_occ
-    charcount = character_occ(get_book_text("books/frankenstein.txt"))
-    print(charcount)
+def display_alph_chars(LISTODIC):
+    for entry in LISTODIC:
+        if entry["char"].isalpha():
+            print(f"{entry["char"]}: {entry["num"]}")
 
+
+def main():
+    filepath = "books/frankenstein.txt"
+    from stats import words_in_book
+    num_words = words_in_book(get_book_text(filepath))
+    from stats import character_occ
+    charcount = character_occ(get_book_text(filepath))
+    from stats import dict_lists
+    print(dict_lists(charcount))
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {filepath}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    display_alph_chars(dict_lists(charcount))
+    print("============= END ===============")
+    
 
 
 main()
